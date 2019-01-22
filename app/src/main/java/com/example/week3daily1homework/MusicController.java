83450969985a96b6f1ce93eff1d2c37e72eeddd8
Joshua <jkvega103@hotmail.com>
2019-01-22 01:57:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week3daily1homework/MusicController.java
@@ -0,0 +1,15 @@
+package com.example.week3daily1homework;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
